cbf8fb3ef2d0271b3e756d61beaf6765406dc0a1
Yusuf Bakır <yusufbkr23@gmail.com>
2023-08-19 00:42:09 +0300

    İsletim sistemi adı ve sürümü

---
+++ isletimSistemi.java
@@ -0,0 +1,9 @@
+class isletimSistemi {
+
+  public static void main(String[] args) {
+
+    String operatingSystem = System.getProperty("os.name");
+    System.out.println(operatingSystem);
+
+  }
+}
