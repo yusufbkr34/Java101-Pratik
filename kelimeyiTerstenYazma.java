336a7eb5e88e8865e819b9a68cba7e4c6498e50d
Yusuf Bakır <yusufbkr23@gmail.com>
2023-08-27 17:02:32 +0300

    Verilen kelimeyi tersten yazma

---
+++ kelimeyiTerstenYazma.java
@@ -0,0 +1,15 @@
+public class kelimeyiTerstenYazma {
+
+  public static void main(String[] args) {
+    String sentence = "Go work";
+    String reversed = reverse(sentence);
+    System.out.println("Tersine cevrilmis kelime: " + reversed);
+  }
+
+  public static String reverse(String sentence) {
+    if (sentence.isEmpty())
+      return sentence;
+
+    return reverse(sentence.substring(1)) + sentence.charAt(0);
+  }
+}
